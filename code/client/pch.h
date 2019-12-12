e61d68b3a73be4dd5ff1edce7ed873e74956c0e5
yuldk <yul.dev.kim@gmail.com>
2019-12-13 00:32:06 +0900

    #0000 init base code
    - add git ignore
    - add cmake
    - add default code

---
+++ code/client/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+
+#include <common/common.h>
