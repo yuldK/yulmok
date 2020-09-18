559d4fffbe1ff1d4938e63debe13d131e5a81948
yuldk <yul.dev.kim@gmail.com>
2020-09-18 21:53:28 +0900

    #0008 processing multithread

--- code/omok/player.cpp
+++ code/omok/player.cpp
@@ -3,6 +3,11 @@
 #include <map>
 #include <random>
 
+#include <thread>
+#include <future>
+#include <execution>
+#include <algorithm>
+
 #include <omok/rule.h>
 
 namespace
@@ -39,18 +44,29 @@ namespace omok
 		using pos_type = coord_type::value_type;
 		std::map<rule::weight_t, std::vector<coord>> recommands;
 
+		std::mutex lock;
+		std::vector<coord> elem;
+
 		for (pos_type x = 0; x < omok_width; ++x)
-		{
 			for (pos_type y = 0; y < omok_height; ++y)
+				elem.emplace_back(x, y);
+
+		std::for_each
+		(
+			std::execution::par_unseq,
+			elem.begin(),
+			elem.end(),
+			[&](const coord& pos)
 			{
-				coord pos = coord{ x, y };
 				if (my_board[pos] != state::space)
-					continue;
+					return;
 
 				rule::weight_t value = rule::check::weight(my_board, pos, my_state);
-				recommands[value].push_back(pos);
+				std::lock_guard<std::mutex> lock_guard{ lock };
+
+				recommands[value].emplace_back(pos);
 			}
-		}
+		);
 
 		const auto& best_pos = recommands.rbegin()->second;
 		return rocommand_pos = best_pos[std::uniform_int_distribution<size_t>{ 0, best_pos.size() - 1}(random_engine)];
